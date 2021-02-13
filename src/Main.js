dfa022d1174027f60113fa75c7606290e2dc5346
khalid-alsheeb <khalid.alsheeb@kcl.ac.uk>
2021-02-14 02:45:45 +0300

    created Main component

---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import { React } from "react";
+import { Switch, Route } from "react-router-dom";
+
+
+const Main = () => {
+    return (
+        <Switch>
+            <Route></Route>
+        </Switch>
+    )
+}
+
+export default Main;
