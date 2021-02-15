0eba3f9a7ecbd85418ed56051b3148c7fb5ec385
Tushita Yadav <tushita.yadav@kcl.ac.uk>
2021-02-15 17:45:20 +0000

    Add Form to create posts

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 import postRoutes from './routes/posts.js';
 
 const app = express();
+
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
