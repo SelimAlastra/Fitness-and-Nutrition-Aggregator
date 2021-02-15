5e24764dd02c16781c91609f82e21ed5c43d8062
Joshua Harris <joshuakharris12@gmail.com>
2021-02-15 13:28:38 +0000

    Define Tags component

---
+++ src/Tags.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Tags extends React.Component {
+    render() {
+        const tags = this.props.tags;
+        for(var i = 0; i < tags.length; ++i) {
+            console.log(tags[i]);
+        }
+        let size = 3;
+        if (tags.length < 3) size = tags.length;
+        let tagString = "";
+        for (var i = 0; i < size; ++i) {
+            tagString += tags[i];
+            if (i < 2) {
+                tagString += " | ";
+            }
+        }
+        return (<div className="title">{tagString}</div>);
+    }
+}
+
+export default Tags;
