dba417b9d99ab5284708fdf69c565cbda2ca32e7
Tushita Yadav <tushita.yadav@kcl.ac.uk>
2021-02-16 13:22:16 +0000

    Added card functionality for Posts

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import './index.css';
 
 //import reportWebVitals from './reportWebVitals';
 
@@ -11,6 +10,7 @@ import thunk from 'redux-thunk';
 import reducers from './reducers';
 
 import App from './App';
+import './index.css';
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
