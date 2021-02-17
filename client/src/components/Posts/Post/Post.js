ddc4c93a6467cb2b56f5ad5b9f45f3c9e8e73877
Bianca Opariuc <opariucbianca@yahoo.ro>
2021-02-17 18:43:51 +0000

    Edit functionality for post'

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -7,7 +7,7 @@ import moment from 'moment';
 
 import useStyles from './styles';
 
-const Post = ({ post }) => {
+const Post = ({ post , setCurrentId }) => {
     const classes = useStyles();
     return (
         <Card className={classes.card}> 
@@ -17,15 +17,17 @@ const Post = ({ post }) => {
                 <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
             </div>
             <div className={classes.overlay2}> 
-                <Button style={{color: 'white'}} size="small" onClick={() => {}}>
+                <Button style={{color: 'white'}} size="small" 
+                onClick={() => setCurrentId(post._id)}>
                     <MoreHorizIcon fontSize="default" />
                 </Button>
             </div>
             <div className={classes.details}> 
                 <Typography variant="body2" color="textSecondary" component="h2">{post.tags.map((tag) => `#${tag} `)}</Typography>
             </div>
+            <Typography className={classes.title} variant="h5" gutterBottom>{post.title}</Typography>
             <CardContent> 
-                <Typography className={classes.title} variant="h5" gutterBottom>{post.message}</Typography>
+                <Typography  variant="h5" gutterBottom>{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}> 
                 <Button size="small" color="primary" onClick={() => {}}>
