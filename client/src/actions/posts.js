038d563172b71234360fbdacd45615d08618277b
Bianca Opariuc <opariucbianca@yahoo.ro>
2021-02-17 18:43:51 +0000

    Edit functionality for post'

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -21,4 +21,13 @@ export const getPosts = () => async (dispatch) => {
       console.log(error.message);
     }
   };
+ 
+export const updatePost = (id,post) => async (dispatch) => {
+  try{
+     const { data} = await api.updatePost(id, post);
 
+     dispatch({ type: 'UPDATE', payload: data});
+  }catch(error){
+    console.log(error);
+  }
+};
