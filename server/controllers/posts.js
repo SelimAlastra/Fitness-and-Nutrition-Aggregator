420c4d7eafd2eb1735bf2d2d1ad457c0e5bac12c
Bianca Opariuc <opariucbianca@yahoo.ro>
2021-02-17 19:25:25 +0000

    Like and Delete Functionality to Post

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -35,3 +35,25 @@ export const updatePost = async (req, res) => {
 
     res.json(updatedPost);
 }
+
+export const deletePost = async (req ,res) =>{
+    const {id} = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: id`);
+
+    await PostMessage.findByIdAndRemove(id);
+
+    res.json({ message :'Post deleted successfully!'});
+}
+
+export const likePost = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+    
+    const post = await PostMessage.findById(id);
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+    
+    res.json(updatedPost);
+}
