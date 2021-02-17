7b75f30f0271a8312d8ee2c925d4c9b95fa63791
Bianca Opariuc <opariucbianca@yahoo.ro>
2021-02-17 19:25:25 +0000

    Like and Delete Functionality to Post

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -4,11 +4,13 @@ import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
-
+import {useDispatch} from 'react-redux';
 import useStyles from './styles';
 
+import {deletePost, likePost} from '../../../actions/posts';
 const Post = ({ post , setCurrentId }) => {
     const classes = useStyles();
+    const dispatch =useDispatch();
     return (
         <Card className={classes.card}> 
             <CardMedia className={classes.media} image={post.selectedFile} title={Post.title} />
@@ -27,15 +29,15 @@ const Post = ({ post , setCurrentId }) => {
             </div>
             <Typography className={classes.title} variant="h5" gutterBottom>{post.title}</Typography>
             <CardContent> 
-                <Typography  variant="h5" gutterBottom>{post.message}</Typography>
+                <Typography  variant="body2" color="textSecondary" component="p" >{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}> 
-                <Button size="small" color="primary" onClick={() => {}}>
+                <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))}>
                     <ThumbUpAltIcon fontSize="small" /> 
-                    Like 
+                    &nbsp; Like &nbsp;
                     {post.likeCount}                    
                 </Button>
-                <Button size="small" color="primary" onClick={() => {}}>
+                <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
                     <DeleteIcon fontSize="small" /> 
                     Delete                   
                 </Button>
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,12 +1,14 @@
 import express from 'express';
 
-import { getPosts, createPost , updatePost} from '../controllers/posts.js';
+import { getPosts, createPost , updatePost, deletePost, likePost} from '../controllers/posts.js';
 
 const router = express.Router();
 
 router.get('/', getPosts);
 router.post('/', createPost);
 router.patch('/:id', updatePost);
+router.delete('/:id', deletePost);
+router.patch('/:id/likePost', likePost);
 /*
 router.get('/:id', getPost);
 router.patch('/:id', updatePost);
