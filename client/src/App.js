7a6413e8246d0eb0f20ace674ba552a644be03c1
Tushita Yadav <tushita.yadav@kcl.ac.uk>
2021-02-19 12:18:53 +0000

    Added mobile responsiveness and action constants

--- client/src/App.js
+++ client/src/App.js
@@ -49,7 +49,7 @@ return (
 
     <Grow in>
         <Container>
-          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+          <Grid className={classes.mainContainer} container justify="space-between" alignItems="stretch" spacing={3}>
             <Grid item xs={12} sm={7}>
               <Posts setCurrentId={setCurrentId}/>
             </Grid>
--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
@@ -12,8 +14,9 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://biancaopariuc:biancaopariuc2021@cluster0.dokic.mongodb.net/<dbname>?retryWrites=true&w=majority';
+//const CONNECTION_URL = 'mongodb+srv://biancaopariuc:biancaopariuc2021@cluster0.dokic.mongodb.net/<dbname>?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
+const CONNECTION_URL = process.env.CONNECTION_URL;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
