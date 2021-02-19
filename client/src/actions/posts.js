8232a4035b8c0a25328bfdc2039fd3ba3c0e659d
Tushita Yadav <tushita.yadav@kcl.ac.uk>
2021-02-19 12:18:53 +0000

    Added mobile responsiveness and action constants

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,4 +1,6 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE } from '../constants/actionTypes';
 import * as api from '../api';
+
 
 // Action Creators
 
@@ -6,7 +8,7 @@ export const getPosts = () => async (dispatch) => {
     try {
       const { data } = await api.fetchPosts();
   
-      dispatch({ type: 'FETCH_ALL', payload: data });
+      dispatch({ type: FETCH_ALL, payload: data });
     } catch (error) {
       console.log(error.message);
     }
@@ -16,7 +18,7 @@ export const getPosts = () => async (dispatch) => {
     try {
       const { data } = await api.createPost(post);
   
-      dispatch({ type: 'CREATE', payload: data });
+      dispatch({ type: CREATE, payload: data });
     } catch (error) {
       console.log(error.message);
     }
@@ -26,7 +28,7 @@ export const updatePost = (id,post) => async (dispatch) => {
   try{
      const { data} = await api.updatePost(id, post);
 
-     dispatch({ type: 'UPDATE', payload: data});
+     dispatch({ type: UPDATE, payload: data});
   }catch(error){
     console.log(error);
   }
@@ -36,7 +38,7 @@ export const deletePost = (id) => async (dispatch) => {
   try {
     await api.deletePost(id);
 
-    dispatch({ type: 'DELETE', payload: id });
+    dispatch({ type: DELETE, payload: id });
   } catch (error) {
     console.log(error.message);
   }
@@ -46,7 +48,7 @@ export const likePost = (id) => async (dispatch) => {
   try {
     const { data } = await api.likePost(id);
 
-    dispatch({ type: 'LIKE', payload: data });
+    dispatch({ type: UPDATE, payload: data });
   } catch (error) {
     console.log(error.message);
   }
