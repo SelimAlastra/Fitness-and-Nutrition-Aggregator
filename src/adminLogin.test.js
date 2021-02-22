e5ea5981e4f2cef650eb02c07b3af32251253e48
khalid-alsheeb <khalid.alsheeb@kcl.ac.uk>
2021-02-22 22:58:32 +0300

    admin login page test

--- src/adminLogin.test.js
+++ src/adminLogin.test.js
@@ -1,11 +1,12 @@
-import { React } from "react";
+import { Component, React } from "react";
 import { shallow, configure } from "enzyme";
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import AdminLogin from "./AdminLogin";
+import { Container } from "react-bootstrap";
 
 configure({ adapter: new Adapter() });
 describe("AdminLogin", () => {
     it("should render admin login page", () => {
-        const wrapper = shallow(<AdminLogin />);
+        const component = shallow(<AdminLogin />)
     });
 });
