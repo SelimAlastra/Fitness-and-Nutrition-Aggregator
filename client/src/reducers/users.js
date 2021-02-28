319f7bf34aaea469be4bbb81e40fbe4ef07b8987
khalid-alsheeb <khalid.alsheeb@kcl.ac.uk>
2021-02-28 03:49:59 +0300

    fixed the ban button 2

--- client/src/reducers/users.js
+++ client/src/reducers/users.js
@@ -1,9 +1,11 @@
 export default (users = [], action) => {
     switch (action.type) {
+        case 'DELETE':
+            return users.filter((user) => user._id !== action.payload);
+        case 'UPDATE': 
+        return users.map((user) => user._id === action.payload._id ? action.payload : user);
         case 'FETCH_ALL':
             return action.payload;
-        case 'UPDATE': 
-            return users.map((user) => user._id === action.payload._id ? action.payload : user);
         case 'CREATE':
             return [ ...users, action.payload ];
         default:
