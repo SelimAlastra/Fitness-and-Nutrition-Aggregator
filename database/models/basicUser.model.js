5dd1d2445937966f6d151a791e10973f80300ffd
Selim Alastra <selim.alastra@kcl.ac.uk>
2021-03-02 12:36:37 +0100

    First version of the database

---
+++ database/models/basicUser.model.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const basicUserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    minlength: 3
+  },
+  name :{type: String,  required: true},
+  //I still don't know how to divide this in first name/ middle name/ last name
+
+  email :{type: String, unique:true, required: true},
+
+  password :{type: String, required: true},
+
+  gender: {type: String, enum: ["Male", "Female", "Other"], required: true},
+
+  dob: {type: Date, required:true},
+
+  adress: {type: String, required:true},
+
+  isBanned: {type: Boolean, required: false},
+
+  bodyType :{type: String,  required: false},
+
+  weight :{type: String,  required: false, default: null},
+
+  goals :[{type: String,  required: false}],
+
+  tags : [{type: String,  required: false}],
+  //I used an array here cause an user may have multiple tags
+
+  bio :{type: String,  required: false},
+
+}, {
+  timestamps: true,
+});
+
+const BasicUser = mongoose.model('BasicUser', basicUserSchema);
+
+module.exports = BasicUser;
---
+++ database/models/service.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const serviceSchema = new Schema({
+
+  userID :{type: Schema.Types.ObjectId, ref: 'ProfessionalUser', required : true},
+  price :{type: String, required: true},
+  description: {type: String ,required : true}
+
+}, {
+  timestamps: true,
+});
+
+const Service = mongoose.model('Service', serviceSchema);
+
+module.exports = Service;
