ff8cd1a5f4e1b4e7c36cc80519b641259f38819c
Joshua Harris <joshuakharris12@gmail.com>
2021-03-06 15:12:09 +0000

    Add validation to form and styling

--- client/src/components/ProfessionalProfile/EditProfessionalDetails.js
+++ client/src/components/ProfessionalProfile/EditProfessionalDetails.js
@@ -8,6 +8,8 @@ import { updateProfessional } from '../../actions/professionals';
 
 const EditProfessionalDetails = (props) => {
     const dispatch = useDispatch();
+    const [validated, setValidated] = useState(false);
+    const [isValidID, setIsValidID] = useState(false);
 
     const [name, setName] = useState("");
     const [address, setAddress] = useState("");
@@ -22,158 +24,175 @@ const EditProfessionalDetails = (props) => {
     const [isBanned, setIsBanned] = useState(false);
     const [tags, setTags] = useState([]);
     const [ID, setID] = useState("");
+    
 
     useEffect(() => {
-        fetch("http://localhost:5000/professionalUsers/" + props.match.params.id)
-        .then(response => response.json())
+        const id = props.match.params.id;
+        fetch(`http://localhost:5000/professionalUsers/${id}`)
+        .then(response => {
+            if (!response.ok) return "error"
+            else return response.json();
+        })
         .then(data => {
-            setName(data.name);
-            setAddress(data.address);
-            setUsername(data.username);
-            setEmail(data.email);
-            setBio(data.bio);
-            setInstagramLink(data.instagramLink);
-            setYoutubeLink(data.youtubeLink);
-            setGender(data.gender);
-            setDob(data.dob);
-            setPassword(data.password);
-            setIsBanned(data.isBanned);
-            setTags(data.tags);
-            setID(data._id);
+            if (data === "error") {
+                setIsValidID(false);
+            } else {
+                setIsValidID(true);
+                setName(data.name);
+                setAddress(data.address);
+                setUsername(data.username);
+                setEmail(data.email);
+                setBio(data.bio);
+                setInstagramLink(data.instagramLink);
+                setYoutubeLink(data.youtubeLink);
+                setGender(data.gender);
+                setDob(data.dob);
+                setPassword(data.password);
+                setIsBanned(data.isBanned);
+                setTags(data.tags);
+                setID(data._id);
+            }
         })
     }, [dispatch]);
 
     function handleSubmit(event) {
-        event.preventDefault();
-        const updatedProfile = {
-            name: name,
-            username: username,
-            address: address,
-            email: email,
-            bio: bio,
-            instagramLink: instagramLink,
-            youtubeLink: youtubeLink,
-            gender: gender,
-            dob: dob,
-            password: password,
-            isBanned: isBanned,
-            tags: tags,
-        }
-        dispatch(updateProfessional(ID, updatedProfile));
-        console.log("Saved");
-        
-
+        if (isValidID) {
+            const editForm = event.currentTarget;
+            if (editForm.checkValidity()) {
+                event.preventDefault();
+                const updatedProfile = {
+                    name: name,
+                    username: username,
+                    address: address,
+                    email: email,
+                    bio: bio,
+                    instagramLink: instagramLink,
+                    youtubeLink: youtubeLink,
+                    gender: gender,
+                    dob: dob,
+                    password: password,
+                    isBanned: isBanned,
+                    tags: tags,
+                }
+                setValidated(true);
+                dispatch(updateProfessional(ID, updatedProfile));
+                window.alert("Details Saved!");
+            } else {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+        } 
     }
 
     return (
-        <div className="formContainer">
-            <Form onSubmit={handleSubmit}>
-                <Form.Group>
-                    <Form.Label className="label">Name</Form.Label>
-                    <Form.Control 
-                        className="inputItem"
-                        id="name" 
-                        name="name" 
-                        value={name} 
-                        placeholder="Name" 
-                        onChange={(e) => setName(e.target.value)} 
-                        required
-                    />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label className="label">Address</Form.Label>
-                    <Form.Control 
-                        className="inputItem"
-                        id="address" 
-                        name="address" 
-                        value={address} 
-                        placeholder="Address" 
-                        onChange={(e) => setAddress(e.target.value)} 
-                        required
-                    />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label className="label">Username</Form.Label>
-                    <Form.Control 
-                        className="inputItem"
-                        id="username" 
-                        name="username" 
-                        value={username} 
-                        placeholder="Username" 
-                        onChange={(e) => setUsername(e.target.value)} 
-                        required 
-                    />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label className="label">Email</Form.Label>
-                    <Form.Control 
-                        className="inputItem"
-                        id="email" 
-                        name="email" 
-                        value={email} 
-                        placeholder="Email" 
-                        onChange={(e) => setEmail(e.target.value)} 
-                        required
-                    />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label className="label">Gender</Form.Label>
-                    <Form.Control 
-                        className="inputItem"
-                        as="select"
-                        id="gender" 
-                        name="gender" 
-                        value={gender} 
-                        placeholder="Gender" 
-                        onChange={(e) => setGender(e.target.value)} 
-                        required
-                    >
-                        <option>Male</option> 
-                        <option>Female</option> 
-                        <option>Other</option> 
-                    </Form.Control>
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label className="label">Bio</Form.Label>
-                    <Form.Control 
-                        className="inputItem"
-                        id="bio" 
-                        as="textarea" 
-                        name="bio" 
-                        value={bio} 
-                        placeholder="Bio" 
-                        onChange={(e) => setBio(e.target.value)} 
-                    />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label className="label">Instagram Username</Form.Label>
-                    <Form.Control 
-                        className="inputItem"
-                        id="instagramLink" 
-                        name="instagramLink" 
-                        value={instagramLink} 
-                        placeholder="Instagram Username" 
-                        onChange={(e) => setInstagramLink(e.target.value)} 
-                    />
-                </Form.Group>
-                <Form.Group>
-                    <Form.Label className="label">Youtube Username</Form.Label>
-                    <Form.Control
-                        className="inputItem" 
-                        id="youtubeLink" 
-                        name="youtubeLink" 
-                        value={youtubeLink} 
-                        placeholder="Youtube Link" 
-                        onChange={(e) => setYoutubeLink(e.target.value)}    
-                    /><br/>
-                    <Button type="submit" className="saveButton">Save</Button>
-                </Form.Group>
-            </Form>
+        <div>
+            <h2 className="title">Edit Details</h2>
+            <div className="formContainer">
+                <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                    <Form.Group>
+                        <Form.Label className="label">Name</Form.Label>
+                        <Form.Control 
+                            className="inputItem"
+                            id="name" 
+                            name="name" 
+                            value={name} 
+                            placeholder="Name" 
+                            onChange={(e) => setName(e.target.value)} 
+                            required
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label className="label">Address</Form.Label>
+                        <Form.Control 
+                            className="inputItem"
+                            id="address" 
+                            name="address" 
+                            value={address} 
+                            placeholder="Address" 
+                            onChange={(e) => setAddress(e.target.value)} 
+                            required
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label className="label">Username</Form.Label>
+                        <Form.Control 
+                            className="inputItem"
+                            id="username" 
+                            name="username" 
+                            value={username} 
+                            placeholder="Username" 
+                            onChange={(e) => setUsername(e.target.value)} 
+                            required 
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label className="label">Email</Form.Label>
+                        <Form.Control 
+                            className="inputItem"
+                            id="email" 
+                            name="email" 
+                            value={email} 
+                            placeholder="Email" 
+                            onChange={(e) => setEmail(e.target.value)} 
+                            required
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label className="label">Gender</Form.Label>
+                        <Form.Control 
+                            className="inputItem"
+                            as="select"
+                            id="gender" 
+                            name="gender" 
+                            value={gender} 
+                            placeholder="Gender" 
+                            onChange={(e) => setGender(e.target.value)} 
+                            required
+                        >
+                            <option>Male</option> 
+                            <option>Female</option> 
+                            <option>Other</option> 
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label className="label">Bio</Form.Label>
+                        <Form.Control 
+                            className="inputItem"
+                            id="bio" 
+                            as="textarea" 
+                            name="bio" 
+                            value={bio} 
+                            placeholder="Bio" 
+                            onChange={(e) => setBio(e.target.value)} 
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label className="label">Instagram Username</Form.Label>
+                        <Form.Control 
+                            className="inputItem"
+                            id="instagramLink" 
+                            name="instagramLink" 
+                            value={instagramLink} 
+                            placeholder="Instagram Username" 
+                            onChange={(e) => setInstagramLink(e.target.value)} 
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label className="label">YouTube Username</Form.Label>
+                        <Form.Control
+                            className="inputItem" 
+                            id="youtubeLink" 
+                            name="youtubeLink" 
+                            value={youtubeLink} 
+                            placeholder="YoutubeLink" 
+                            onChange={(e) => setYoutubeLink(e.target.value)}    
+                        /><br/>
+                        <Button type="submit" className="saveButton">Save</Button>
+                    </Form.Group>
+                </Form>
+            </div>
         </div>
-    );
-
-
-    
+    ); 
 }
 
 export default EditProfessionalDetails;
