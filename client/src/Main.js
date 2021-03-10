ac99a6e1548b6fd69f6fd0113c806e88998cc2f0
Ioana Ghinea <72256087+ioanaghinea@users.noreply.github.com>
2021-03-10 18:13:13 +0000

    Refactor client register and login.
    Link with the database. Implement forgot and reset password. Implement Facebook and Google login.

--- client/src/Main.js
+++ client/src/Main.js
@@ -1,11 +1,14 @@
-import { React } from "react";
+import React, { Fragment } from "react";
 import { Switch, Route } from "react-router-dom";
 import AdminLogin from "./AdminLogin";
 import AdminPage from "./AdminPage";
 import LandingPage from './LandingPage';
 import GeneralDashboard from './GeneralDashboard';
-
-
+import Register from './components/BasicUsersAuth/userRegister.jsx';
+import Login from './components/BasicUsersAuth/userLogin.jsx';
+import UserPage from './components/BasicUsersAuth/userPage.jsx';
+import ForgetPassword from './components/BasicUsersAuth/forgetPassword.jsx';
+import ResetPassword from './components/BasicUsersAuth/resetPassword.jsx';
 
 const Main = () => {
     let logged = false;
@@ -16,6 +19,10 @@ const Main = () => {
                 <Route exact path='/adminLogin' component={AdminLogin}></Route>
                 <Route exact path='/adminPage' component={AdminPage}></Route>
                 <Route exact path='/generalDashboard' component={GeneralDashboard}></Route>
+                <Route exact path='/launch' exact render={props => <Fragment> <Login {...props}/> <Register {...props}/> </Fragment>} />
+                <Route exact path='/user' exact render={props => <UserPage {...props} />} />
+                <Route exact path='/user/password/forget' exact render={props => <ForgetPassword {...props} />} />
+                <Route exact path='/user/password/reset/:token' exact render={props => <ResetPassword {...props} />} />
             </Switch>
         )
     else return (
