b318adee643f76cfa113f604dc91d17ad58c5c90
Ioana Ghinea <72256087+ioanaghinea@users.noreply.github.com>
2021-03-10 19:13:17 +0000

    Deleted everything in order to refactor professional register and login.

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import logo from './logo.svg';
-import './App.css';
-import ProfessionalSignup from "./professional_signup.js";
-
-function App() {
-  return < ProfessionalSignup />;
-}
-
-export default App;
