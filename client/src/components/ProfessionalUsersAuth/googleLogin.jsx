a1d398d866e38852d36aef0d65ee5976a77eba95
Ioana Ghinea <72256087+ioanaghinea@users.noreply.github.com>
2021-03-10 20:42:46 +0000

    Refactor professional login and register
    Implement Facebook and Google login. Link with the database. Implement forget and reset password functionality.

---
+++ client/src/components/ProfessionalUsersAuth/googleLogin.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import {GoogleLogin} from 'react-google-login';
+import axios from 'axios';
+import { authenticate, isAuth } from '../../actions/userAuth.js';
+import { useHistory } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGoogle } from '@fortawesome/free-brands-svg-icons' 
+import './googleLogin.css';
+
+const Google = () => {
+    const history = useHistory()
+
+    const icon = <FontAwesomeIcon icon={faGoogle} />
+
+const sendGoogleToken = tokenId => {
+  axios
+    .post(`http://localhost:5000/professionalUsers/googlelogin`, {
+      idToken: tokenId
+    })
+    .then(res => {
+      console.log(res.data);
+      informParent(res);
+    })
+    .catch(error => {
+      console.log('GOOGLE SIGN IN ERROR', error.response);
+    });
+};
+
+const informParent = response => {
+  authenticate(response, () => {
+    isAuth() 
+    history.push('/professional');
+  });
+};
+
+const responseGoogle = response => {
+  console.log(response);
+  sendGoogleToken(response.tokenId);
+};
+    return (
+        <GoogleLogin
+          clientId='490602914779-7kv3ltvaluhh4ca4f3p7skuhv065dn6a.apps.googleusercontent.com'
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+          render={renderProps => (
+            <button
+              onClick={renderProps.onClick}
+              disabled={renderProps.disabled}
+              className='GoogleLogin'
+              >
+              <div className=' p-2 rounded-full '>
+                <i style={{'marginLeft': '5px'}}>{icon}</i>
+              </div>
+              <span style={{'marginTop': '8px', 'marginLeft': '8px' }}>Sign In with Google</span>
+            </button>
+          )}
+          cookiePolicy={'single_host_origin'}
+        />
+    );
+  };
+
+  export default Google;
