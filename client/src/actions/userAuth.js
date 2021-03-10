2aca3fb6424ada10115b780612fb7a90e6647aba
Ioana Ghinea <72256087+ioanaghinea@users.noreply.github.com>
2021-03-10 18:13:13 +0000

    Refactor client register and login.
    Link with the database. Implement forgot and reset password. Implement Facebook and Google login.

---
+++ client/src/actions/userAuth.js
@@ -0,0 +1,79 @@
+import cookie from 'js-cookie';
+
+export const setCookie = (key, value) => {
+    if(window !== 'undefined'){
+        cookie.set(key, value, {
+            expires: 1
+        })
+    }
+}
+
+//Remove from cookie
+export const removeCookie = key => {
+    if(window !== 'undefined'){
+        cookie.remove(key, {
+            expires: 1
+        })
+    }
+}
+
+export const getCookie = key => {
+    if (window !== 'undefined'){
+        cookie.get(key, {
+            expires: 1
+        })
+    }
+}
+
+export const setLocalStorage  = (key, value) => {
+    if (window !== 'undefined') {
+        localStorage.setItem(key, JSON.stringify(value));
+    }
+}
+
+
+// Remove from localstorage
+export const removeLocalStorage = key => {
+    if (window !== 'undefined') {
+        localStorage.removeItem(key);
+    }
+};
+
+// Authenticate user by passing data to cookie and localStorage during signin
+export const authenticate = (response, next) => {
+    console.log('AUTHENTICATE HELPER ON SIGNIN RESPONSE', response);
+    setCookie('token', response.data.token);
+    setLocalStorage('user', response.data.user);
+    next();
+};
+
+// Access user info from localstorage
+export const isAuth = () => {
+    if (window !== 'undefined') {
+        const cookieChecked = getCookie('token');
+        if (cookieChecked) {
+            if (localStorage.getItem('user')) {
+                return JSON.parse(localStorage.getItem('user'));
+            } else {
+                return false;
+            }
+        }
+    }
+};
+
+export const signOut = next => {
+    removeCookie('token');
+    removeLocalStorage('user');
+    next();
+};
+
+
+export const updateUser = (response, next) => {
+    console.log('UPDATE USER IN LOCALSTORAGE HELPERS', response);
+    if (typeof window !== 'undefined') {
+        let auth = JSON.parse(localStorage.getItem('user'));
+        auth = response.data;
+        localStorage.setItem('user', JSON.stringify(auth));
+    }
+    next();
+};
