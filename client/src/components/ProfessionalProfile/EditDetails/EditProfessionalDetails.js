c49778eda5e61d09e0ac39be9acf36f16c11a056
Joshua Harris <joshuakharris12@gmail.com>
2021-03-11 12:09:53 +0000

    Fix details not updating after save issue

--- client/src/components/ProfessionalProfile/EditDetails/EditProfessionalDetails.js
+++ client/src/components/ProfessionalProfile/EditDetails/EditProfessionalDetails.js
@@ -1,14 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Form, Button, Option } from "react-bootstrap";
+import { Form, Button, Option, Col } from "react-bootstrap";
 import React, { useEffect, useState } from 'react';
-import { useDispatch  } from 'react-redux';
+import { useDispatch, useSelector  } from 'react-redux';
 import "./EditProfessionalDetails.css";
-import { updateProfessional } from '../../../actions/professionals';
+import { updateProfessional, getProfessional } from '../../../actions/professionals';
 
 const EditProfessionalDetails = (props) => {
     const dispatch = useDispatch();
     const [validated, setValidated] = useState(false);
-    const [isValidID, setIsValidID] = useState(false);
     const [name, setName] = useState("");
     const [username, setUsername] = useState("");
     const [address, setAddress] = useState("");
@@ -23,70 +22,58 @@ const EditProfessionalDetails = (props) => {
     const [tags, setTags] = useState([]);
     const [ID, setID] = useState("");
     const [profession, setProfession] = useState("");
+    let professional;
+    useEffect(() => {
+        dispatch(getProfessional(props.match.params.id));
+    }, [props]);
+
+    professional = useSelector((state) => state.professional)
 
     useEffect(() => {
-        const id = props.match.params.id;
-        if (id !== undefined) {
-            fetch(`http://localhost:5000/professionalUsers/${id}`)
-            .then(response => {
-                if (!response.ok) return "error"
-                else return response.json();
-            })
-            .then(data => {
-                if (data === "error") {
-                    setIsValidID(false);
-                } else {
-                    setIsValidID(true);
-                    setName(data.name);
-                    setAddress(data.address);
-                    setUsername(data.username);
-                    setEmail(data.email);
-                    setBio(data.bio);
-                    setInstagramLink(data.instagramLink);
-                    setYoutubeLink(data.youtubeLink);
-                    setGender(data.gender);
-                    setDob(data.dob);
-                    setPassword(data.password);
-                    setIsBanned(data.isBanned);
-                    setTags(data.tags);
-                    setID(data._id);
-                    setProfession(data.profession);
-                }
-            })
-        }
-        
-    }, [dispatch]);
+        setName(professional.name);
+        setAddress(professional.address);
+        setUsername(professional.username);
+        setEmail(professional.email);
+        setBio(professional.bio);
+        setInstagramLink(professional.instagramLink);
+        setYoutubeLink(professional.youtubeLink);
+        setGender(professional.gender);
+        setDob(professional.dob);
+        setPassword(professional.password);
+        setIsBanned(professional.isBanned);
+        setTags(professional.tags);
+        setID(professional._id);
+        setProfession(professional.profession);
+    }, [professional]);
 
     function handleSubmit(event) {
-        if (isValidID) {
-            const editForm = event.currentTarget;
-            if (editForm.checkValidity()) {
-                event.preventDefault();
-                const updatedProfile = {
-                    name: name,
-                    username: username,
-                    address: address,
-                    email: email,
-                    bio: bio,
-                    instagramLink: instagramLink,
-                    youtubeLink: youtubeLink,
-                    gender: gender,
-                    dob: dob,
-                    password: password,
-                    isBanned: isBanned,
-                    tags: tags,
-                    profession: profession,
-                }
-                setValidated(true);
-                dispatch(updateProfessional(ID, updatedProfile));
-                console.log("Done");
-                window.alert("Details Saved!");
-                window.location.href = `/professional/profile/${ID}`
-            } else {
-                event.preventDefault();
-                event.stopPropagation();
+        const editForm = event.currentTarget;
+        if (editForm.checkValidity()) {
+            event.preventDefault();
+            const updatedProfile = {
+                name: name,
+                username: username,
+                address: address,
+                email: email,
+                bio: bio,
+                instagramLink: instagramLink,
+                youtubeLink: youtubeLink,
+                gender: gender,
+                dob: dob,
+                password: password,
+                isBanned: isBanned,
+                tags: tags,
+                profession: profession,
             }
-        } 
+            setValidated(true);
+            dispatch(updateProfessional(ID, updatedProfile));
+            console.log("Done");
+            window.alert("Details Saved!");
+            window.location.href = `/professional/profile/${ID}`
+        } else {
+            event.preventDefault();
+            event.stopPropagation();
+        }
     }
 
     return (
@@ -95,18 +82,37 @@ const EditProfessionalDetails = (props) => {
             <hr className="seperator"/>
             <div>
                 <Form noValidate validated={validated} onSubmit={handleSubmit}>
-                    <Form.Group>
-                        <Form.Label className="label">Name</Form.Label>
-                        <Form.Control 
-                            className="inputItem"
-                            id="name" 
-                            name="name" 
-                            value={name} 
-                            placeholder="Name" 
-                            onChange={(e) => setName(e.target.value)} 
-                            required
-                        />
-                    </Form.Group>
+                    <Form.Row>
+                        <Col>
+                            <Form.Group>
+                                <Form.Label className="label">Name</Form.Label>
+                                <Form.Control 
+                                    className="inputItem"
+                                    id="name" 
+                                    name="name" 
+                                    value={name} 
+                                    placeholder="Name" 
+                                    onChange={(e) => setName(e.target.value)} 
+                                    required
+                                />
+                        </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group>
+                                <Form.Label className="label">Username</Form.Label>
+                                <Form.Control 
+                                    className="inputItem"
+                                    id="username" 
+                                    name="username" 
+                                    value={username} 
+                                    placeholder="Username" 
+                                    onChange={(e) => setUsername(e.target.value)} 
+                                    required 
+                                />
+                            </Form.Group>
+                        </Col>
+                    </Form.Row>
+                    
                     <Form.Group>
                         <Form.Label className="label">Address</Form.Label>
                         <Form.Control 
@@ -119,18 +125,7 @@ const EditProfessionalDetails = (props) => {
                             required
                         />
                     </Form.Group>
-                    <Form.Group>
-                        <Form.Label className="label">Username</Form.Label>
-                        <Form.Control 
-                            className="inputItem"
-                            id="username" 
-                            name="username" 
-                            value={username} 
-                            placeholder="Username" 
-                            onChange={(e) => setUsername(e.target.value)} 
-                            required 
-                        />
-                    </Form.Group>
+                    
                     <Form.Group>
                         <Form.Label className="label">Email</Form.Label>
                         <Form.Control 
@@ -172,29 +167,37 @@ const EditProfessionalDetails = (props) => {
                             onChange={(e) => setBio(e.target.value)} 
                         />
                     </Form.Group>
+                    <Form.Row>
+                        <Col>
+                            <Form.Group>
+                                <Form.Label className="label">Instagram Username</Form.Label>
+                                <Form.Control 
+                                    className="inputItem"
+                                    id="instagramLink" 
+                                    name="instagramLink" 
+                                    value={instagramLink} 
+                                    placeholder="Instagram Username" 
+                                    onChange={(e) => setInstagramLink(e.target.value)} 
+                                />
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group>
+                                <Form.Label className="label">YouTube Username</Form.Label>
+                                <Form.Control
+                                    className="inputItem" 
+                                    id="youtubeLink" 
+                                    name="youtubeLink" 
+                                    value={youtubeLink} 
+                                    placeholder="YoutubeLink" 
+                                    onChange={(e) => setYoutubeLink(e.target.value)}    
+                                /><br/>
+                            </Form.Group>
+                        </Col>
+                    </Form.Row>
                     <Form.Group>
-                        <Form.Label className="label">Instagram Username</Form.Label>
-                        <Form.Control 
-                            className="inputItem"
-                            id="instagramLink" 
-                            name="instagramLink" 
-                            value={instagramLink} 
-                            placeholder="Instagram Username" 
-                            onChange={(e) => setInstagramLink(e.target.value)} 
-                        />
-                    </Form.Group>
-                    <Form.Group>
-                        <Form.Label className="label">YouTube Username</Form.Label>
-                        <Form.Control
-                            className="inputItem" 
-                            id="youtubeLink" 
-                            name="youtubeLink" 
-                            value={youtubeLink} 
-                            placeholder="YoutubeLink" 
-                            onChange={(e) => setYoutubeLink(e.target.value)}    
-                        /><br/>
-                        <Button type="submit" className="actionButton">Save</Button>
-                        <Button type="button" className="actionButton" onClick={() => window.location.href=`/professional/profile/${ID}`}>Close</Button>
+                    <Button type="submit" className="actionButton">Save</Button>
+                                <Button type="button" className="actionButton" onClick={() => window.location.href=`/professional/profile/${ID}`}>Close</Button>
                     </Form.Group>
                 </Form>
             </div>
