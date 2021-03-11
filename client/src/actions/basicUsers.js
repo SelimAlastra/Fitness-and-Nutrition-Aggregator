47dde33088898271f57088e78f0b6f2c21859543
Joshua Harris <joshuakharris12@gmail.com>
2021-03-11 11:35:46 +0000

    Add form to edit user details and save updated details to database

--- client/src/actions/basicUsers.js
+++ client/src/actions/basicUsers.js
@@ -3,9 +3,20 @@ import * as api from '../api';
 export const getBasicUser = (id) => async (dispatch) => {
     try {
         const { data } = await api.getBasicUser(id);
-
-        dispatch({type: 'FETCH', payload: data});
+        
+        dispatch( {type: 'FETCH', payload: data} );
     } catch (error) {
         console.log(error.message);
     }
 }
+
+export const updateBasicUser = (id, newUser) => async (dispatch) => {
+    try {
+        const { data } = await api.updateBasicUser(id, newUser);
+
+        dispatch( {type: 'UPDATE', payload: data} );
+    } catch (error) {
+        console.log("Oh no something went wrong");
+    }
+}
+
