8c5225a21f1a4745a60d0fb136127e2248effd9d
SelimAlastra <alastraselim28@yahoo.fr>
2021-03-11 18:52:55 +0100

    Adds basic components of the HomePage in client

---
+++ client/src/HomePage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Button , Alert, Container, Carousel } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './HomePage.css';
+
+function HomePage () {
+    return (
+        <div className = 'homepage'> 
+
+                <Alert className = 'alert1'> Welcome to the Home Page ! </Alert>
+                
+        </div>
+     );
+}
+
+export default HomePage;
