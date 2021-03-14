72ec22e180926fa5841827df90d88e9d7e881182
Joshua Harris <joshuakharris12@gmail.com>
2021-03-14 14:29:28 +0000

    Loads services from basicUser record

--- client/src/Main.js
+++ client/src/Main.js
@@ -25,7 +25,6 @@ import EditServices from "./components/ProfessionalProfile/EditServices/EditServ
 import ProfessionalProfile from './components/ProfessionalProfile/ProfessionalProfile';
 import EditBasicUser from './components/EditBasicUser/EditBasicUser';
 import MyServices from './components/MyServices/MyServices';
-import ServiceVideos from './components/MyServices/ServiceVideos';
 import AddService from './components/ProfessionalProfile/EditServices/AddService';
 
 
@@ -59,7 +58,6 @@ const Main = () => {
                 <Route exact path="/user/edit/:id" component={EditBasicUser}></Route>
                 <Route exact path="/user/myservices/:id" component={MyServices}></Route>
                 <Route exact path="/professional/services/add/:id" component={AddService}></Route>
-                <Route exact path="/user/myservices/videos/:id" component={ServiceVideos}></Route>
         
             </Switch>
         )
