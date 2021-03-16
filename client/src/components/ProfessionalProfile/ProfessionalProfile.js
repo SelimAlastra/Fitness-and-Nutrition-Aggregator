a849262d0a80cc5a394f8731a387a7bcc8d51fc3
Joshua Harris <joshuakharris12@gmail.com>
2021-03-16 12:31:33 +0000

    Chnage services in professional profile to allow for different views

--- client/src/components/ProfessionalProfile/ProfessionalProfile.js
+++ client/src/components/ProfessionalProfile/ProfessionalProfile.js
@@ -5,22 +5,73 @@ import Services from './Services/Services';
 import Thumbnails from "./Thumbnails/Thumbnails";
 import { useDispatch, useSelector } from 'react-redux';
 import { getProfessional } from '../../actions/professionals';
-import { Link } from 'react-router-dom';
+import { getServices } from '../../actions/services';
 
 const ProfessionalProfile = (props) => {
     const dispatch = useDispatch();
     const [videoUrls, setVideoUrls] = useState([]);
-    const [isProfessional, setIsProfessional] = useState(true);
+    const [isProfessional, setIsProfessional] = useState(props.isProfessional);
+    const [basicUserID, setBasicUserID] = useState("");
+    const userID = props.match.params.id;
 
     useEffect(() => {
-        dispatch(getProfessional(props.match.params.id));
+        dispatch(getProfessional(userID));
     }, [dispatch]);
-  
+
     const profile = useSelector((state) => state.professional);
+
+    useEffect(() => {
+        dispatch(getServices());
+
+    }, [dispatch]);
+
+    const services = useSelector((state) => state.services);
+    const myServices = services.filter(service => service.userID === userID);
+  
 
     function generateEditDetailsLink(isProfessional) {
         if (isProfessional) {
             return (<h5 className="editLink" onClick={() => window.location.href = `/professional/profile/edit/${profile._id}`}>Edit my details</h5>);
+        }
+    }
+
+    function generateEditServicesLink(isProfessional) {
+        if (isProfessional) {
+            return (<h5 className="editLink" onClick={() => window.location.href = `/professional/services/edit/${userID}`}>Edit my services</h5>);
+        } 
+    }
+
+    function generateServices() {
+        if (isProfessional) {
+           return (
+               <div>
+                    { generateEditServicesLink(isProfessional) }
+                    <h2 className="pageText">Services</h2>
+                   <ul>
+                       {
+                           myServices.map((service, index) => {
+                               return (
+                                    <li className="serviceList">
+                                        <div className="serviceColumn">
+                                            <h4>{service.title}</h4>
+                                            <p>{service.description}</p>
+                                        </div>
+                                        <div className="serviceColumn">
+                                            <Button>Purchase Bundle</Button>
+                                        </div>
+                                   </li>
+                               )
+                           })
+                       }
+                   </ul>
+               </div>
+           );
+        } else {
+            if (basicUserID !== undefined && basicUserID !== "") {
+                // 
+            } else {
+                // 
+            }
         }
     }
 
@@ -33,10 +84,13 @@ const ProfessionalProfile = (props) => {
                 <ProfileInfo profile={profile} />
             </div>
             <div className="section">
-                <Services 
+                {/* <Services 
                 userID={profile._id}
                 isProfessional={isProfessional}
-                />
+                /> */}
+                {
+                    generateServices()
+                }
             </div>
             <div className="section">
                 <h2 className="pageText">Most Viewed Videos</h2>
