04dbdc7be02ffd864ce8c071bb666db6ab57414e
Joshua Harris <joshuakharris12@gmail.com>
2021-03-18 16:11:44 +0000

    Update css for edit services component

--- client/src/components/ProfessionalProfile/EditServices/AddService.js
+++ client/src/components/ProfessionalProfile/EditServices/AddService.js
@@ -32,77 +32,80 @@ const AddService = (props) => {
     }
 
     return (
-        <div className="formContainer">
-            <div>
-                <h4 className="serviceText">Add Service</h4>
-                <hr className="seperator"/>
+        <div>
+            <div className="formContainer">
                 <div>
-                    <Form>
-                        <Form.Group>
-                            <Form.Label>Title</Form.Label>  
-                            <Form.Control
-                                value={newService.title}
-                                id="titleInput"
-                                name="title" 
-                                placeholder="Title"
-                                className="inputItem" 
-                                onChange={(e) => setNewService({
-                                    ...newService,
-                                    title: e.target.value
-                            })}
-                            >
-                            </Form.Control>
-                        </Form.Group>
-                        <Form.Group>
-                            <Form.Label>Description</Form.Label>  
-                            <Form.Control
-                                value={newService.description}
-                                id="descriptionInput"
-                                name="description" 
-                                placeholder="Description"
-                                className="inputItem" 
-                                onChange={(e) => setNewService({
-                                    ...newService,
-                                    description: e.target.value
-                            })}
-                            >
-                            </Form.Control>
-                        </Form.Group>
-                        <Form.Group>
-                            <Form.Label>Price</Form.Label> 
-                            <Form.Control
-                                value={newService.price}
-                                id="priceInput"
-                                name="price" 
-                                placeholder="Price"
-                                className="inputItem" 
-                                onChange={(e) => setNewService({
-                                    ...newService,
-                                    price: e.target.value
+                    <h4 className="serviceText">Add Service</h4>
+                    <hr className="seperator"/>
+                    <div>
+                        <Form>
+                            <Form.Group>
+                                <Form.Label>Title</Form.Label>  
+                                <Form.Control
+                                    value={newService.title}
+                                    id="titleInput"
+                                    name="title" 
+                                    placeholder="Title"
+                                    className="inputItem" 
+                                    onChange={(e) => setNewService({
+                                        ...newService,
+                                        title: e.target.value
                                 })}
+                                >
+                                </Form.Control>
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Label>Description</Form.Label>  
+                                <Form.Control
+                                    value={newService.description}
+                                    id="descriptionInput"
+                                    name="description" 
+                                    placeholder="Description"
+                                    className="inputItem" 
+                                    onChange={(e) => setNewService({
+                                        ...newService,
+                                        description: e.target.value
+                                })}
+                                >
+                                </Form.Control>
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Label>Price</Form.Label> 
+                                <Form.Control
+                                    value={newService.price}
+                                    id="priceInput"
+                                    name="price" 
+                                    placeholder="Price"
+                                    className="inputItem" 
+                                    onChange={(e) => setNewService({
+                                        ...newService,
+                                        price: e.target.value
+                                    })}
+                                >
+                                </Form.Control>
+                            </Form.Group>
+                        </Form>
+                        <div >
+                            <Button
+                                className="actionButton"
+                                type="submit"
+                                onClick={(event) => addNewService(event)} 
                             >
-                            </Form.Control>
-                        </Form.Group>
-                    </Form>
-                    <div >
-                        <Button
-                            className="actionButton"
-                            type="submit"
-                            onClick={(event) => addNewService(event)} 
-                        >
-                        Save
-                        </Button>
-                        <Button
-                            className="actionButton"
-                            type="button"
-                            onClick={(event) => window.location.href = `/professional/profile/${userID}`} 
-                        >
-                        Close
-                        </Button>
+                            Save
+                            </Button>
+                            <Button
+                                className="actionButton"
+                                type="button"
+                                onClick={(event) => window.location.href = `/professional/profile/${userID}`} 
+                            >
+                            Close
+                            </Button>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+
     );
 }
 
