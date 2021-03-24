85f82bb97baeb3df10f1bd94d6103a635a7f2818
sergiu-stefan <sergiu-stefan.tomescu@kcl.ac.uk>
2021-03-24 12:12:09 +0200

    The quiz array can receive different parameters.

--- client/src/quiz/quizUser.js
+++ client/src/quiz/quizUser.js
@@ -4,145 +4,19 @@ import Answer from './components/answer';
 import Test from './testPage';
 import './styling/quizUser.css';
 
-//const history = useHistory();
+import {questions,questionsReqInput,questionsMultipleChoices} from './resources/clientQuestions';
 
 var associatedTags = [];
 
 export default class Quiz extends Component{
 
     state = {
-        questions: [
-            {
-                questionText: "Are you a woman or a man?",
-                questionId: 1,
-                answerOptions: [
-                    { answerText: "Woman", selected:false, requireInput: false, tags:["womensfitness"] },
-                    { answerText: "Man", selected:false, requireInput: false, tags:["menfitness"] },
-                ],
-            },{
-                questionText: "What is your age range?",
-                questionId: 2,
-                answerOptions: [
-                    { answerText: "Teens", selected:false, requireInput: false, tags:[""] },
-                    { answerText: "20's", selected:false, requireInput: false, tags:[""] },
-                    { answerText: "30's", selected:false, requireInput: false, tags:[""] },
-                    { answerText: "40's", selected:false, requireInput: false, tags:[""] },
-                    { answerText: "50's", selected:false, requireInput: false, tags:[""] },
-                    { answerText: "60+", selected:false, requireInput: false, tags:[""] },
-                ],
-            },{
-                questionText: "What is your height?",
-                questionId: 3,
-                answerOptions: [
-                    { answerText: "metric", selected:true, requireInput: false, tags:[""]  },
-                    { answerText: "imperial", selected:false, requireInput: false, tags:[""] },
-                ],
-                input:[], placeholder: ["cm", "feet, inches"], alert: ["Type only numeric values. (e.g.: 177)", "Type numeric values. (e.g.: 5 8, 6 2)"]
-            },{
-                questionText: "What is your weight?",
-                questionId: 4,
-                answerOptions: [
-                    { answerText: "Kilograms(kg)", selected:true, requireInput: false, tags:[""] },
-                    { answerText: "Pounds(lb)", selected:false, requireInput: false, tags:[""] },
-                ],
-                input:[], placeholder: ["kg","lb"], alert: ["Type only numeric values. (e.g.: 82)", "Type only numeric values. (e.g.: 160)"]
-            },{
-                questionText: "What best describes your body?",
-                questionId: 5,
-                answerOptions: [
-                    { answerText: "Lean and long", selected:false, tags:["tall", "ectomorph"], requireInput: false },
-                    { answerText: "Tall and heavy", selected:false, tags:["tall", "endomorph", "bodybuilding"], requireInput: false },
-                    { answerText: "Athletic, with a high metabolism", selected:false, tags:["athletic", "mesomorph"], requireInput: false },
-                    { answerText: "Soft and with a tendency to store body fat", selected:false, tags:["endomorph", "fatloss"], requireInput: false },
-                    { answerText: "Average", selected:false, tags:["average"], requireInput: false },
-                    { answerText: "Other:", selected:false, tags:[""], requireInput: true, placeholder: "e.g.: Fragile", alert: ["Answer using letters only."] }
-                ],
-                // input:[]
-            },{
-                questionText: "Which best describes your current activity level?",
-                questionId: 6,
-                answerOptions: [
-                    { answerText: "minimum physical activity", selected:false, tags:["beginner"], requireInput: false },
-                    { answerText: "generally active, but not fitness related", selected:false, tags:["beginner"], requireInput: false },
-                    { answerText: "exercise 2-4 times/week", selected:false, tags:["intermediate"], requireInput: false },
-                    { answerText: "sweat it out on a daily basis", selected:false, tags:["advanced"], requireInput: false },
-                ],
-            },{
-                questionText: "Which best describes your current diet?",
-                questionId: 7,
-                answerOptions: [
-                    { answerText: "can't seem to find my appetite", selected:false, tags:["anorexic", "nutrition"], requireInput: false },
-                    { answerText: "I snack a little too much", selected:false, tags:["dietplan", "nutrition", "fatloss"], requireInput: false },
-                    { answerText: "avoid highly processed snacks, but enjoy cheat days", selected:false, tags:["healthylifestyle"], requireInput: false },
-                    { answerText: "healthy and balanced diet", selected:false, tags:["healthylifestyle"], requireInput: false },
-                    { answerText: "Other:", selected: false, tags:[""], requireInput: true, placeholder: "e.g.: Vegetarian", alert: ["Answer using letters only."] }
-                ],
-                // input:[]
-            },{
-                questionText: "What's your primary fitness goal? (multiple selections allowed)",
-                questionId: 8,
-                answerOptions: [
-                    { answerText: "lose fat", selected:false, tags:["losefat", "weightloss", "fatlosstips", "diet", "weightlossjourney", "sweat"], requireInput: false },
-                    { answerText: "get fit/look great at the beach", selected:false, tags:["getfit", "cardio", "fitnessmodel", "crossfit"], requireInput: false },
-                    { answerText: "recover from an injury", selected:false, tags:[""], requireInput: false },
-                    { answerText: "get some nice legs and booty", selected:false, tags:[""], requireInput: false },
-                    { answerText: "build endurance/run a marathon", selected:false, tags:[""], requireInput: false },
-                    { answerText: "fitness as a social activity", selected:false, tags:[""], requireInput: false },
-                    { answerText: "train discipline and focus", selected:false, tags:[""], requireInput: false },
-                    { answerText: "build strength and ability", selected:false, tags:["calisthenics", "bodyweight", "bodybuilding", "crossfit", "strength", "athlete", "core"], requireInput: false },
-                    { answerText: "build mad muscle mass", selected:false, tags:["beastmode", "bodybuilding", "strength", "powerlifting", "weightlifting", "strongman"], requireInput: false },
-                    // { answerText: "Other:", selected: false, tags:[""], requireInput: true, placeholder: "e.g.: do 100 pushups in one set", alert: ["Answer using letters and numbers only."] }
-                ],
-                // input:[]
-            },{
-                questionText: "During the average week, how much time are you able to devote to your fitness regimen?",
-                questionId: 9,
-                answerOptions: [
-                    { answerText: "barely 15-30 min on any given day", selected:false, tags:[""], requireInput: false },
-                    { answerText: "between 30 min and one hour per day, 3 days a week", selected:false, tags:[""], requireInput: false },
-                    { answerText: "around one hour per day, 3 days a week", selected:false, tags:[""], requireInput: false },
-                    { answerText: "around one hour per day on most days", selected:false, tags:[""], requireInput: false },
-                    { answerText: "more than one hour per day on most days", selected:false, tags:[""], requireInput: false },
-                ],
-                // input:[]
-            },{ 
-                questionText: "Where is your favorite place to exercise?",
-                questionId: 10,
-                answerOptions: [
-                    { answerText: "gym", selected:false, tags:[""], requireInput: false },
-                    { answerText: "outdoors", selected:false, tags:["outdoor", "outdoortraining", "outdoorworkout", "nature"], requireInput: false },
-                    { answerText: "home", selected:false, tags:["home", "homeworkout", "homegym", "quarantine", "nogymnoproblem"], requireInput: false },
-                ],
-            },{ 
-                questionText: "How much are you willing to spend? (gym membership, gear & equipment)",
-                questionId: 11,
-                answerOptions: [
-                    { answerText: "not much", selected:false, tags:["nocost"], requireInput: false },
-                    { answerText: "a fair amount", selected:false, tags:[""], requireInput: false },
-                    { answerText: "no budget limit", selected:false, tags:["expensive"], requireInput: false },
-                    // { answerText: "Other(per month):", selected: false, tags:[""], requireInput: true, placeholder: "e.g.: 100 pounds", alert: ["Answer using letters and numbers only."] }
-                ],
-                // input:[]
-            },{ 
-                questionText: "Lastly, which of the following are you interested in?",
-                questionId: 12,
-                answerOptions: [
-                    { answerText: "#mindset", selected:true, tags:["mindset", "motivation", "goals"], requireInput: false },
-                    { answerText: "#nutrition", selected:false, tags:["nutrition", "diet"], requireInput: false },
-                    { answerText: "#jogging", selected:false, tags:["jogging", "running"], requireInput: false },
-                    { answerText: "#bodyweightworkout", selected:false, tags:["bodyweightworkou", "calisthenics"], requireInput: false },
-                    { answerText: "#bodybuilding", selected:false, tags:["bodybuilding", "gains", "muscle"], requireInput: false },
-                    { answerText: "#powerlifting", selected:false, tags:["powerlifting", "deadlifting"], requireInput: false },
-                    // { answerText: "Add your own:", selected: false, tags:[""], requireInput: true, placeholder: "e.g.: crossfit, boxing", alert: ["Answer using letters and numbers only."] }
-                ],
-                // input:[]
-            }
-        ],
+        questions: questions,
 
         // identify questions that require input for all selections by adding their ID in this array
-        questionsReqInput: [3,4],
+        questionsReqInput: questionsReqInput,
         // identify questions that accept multiple selections by adding their ID in this array
-        questionsMultipleChoices:[8,12],
+        questionsMultipleChoices: questionsMultipleChoices,
 
         currentQuestion: 0,
         complete: false
