5a5fc344c008111e16678e7bd5c7ffc1f5728361
Joshua Harris <joshuakharris12@gmail.com>
2021-03-29 16:18:25 +0100

    Fix serivces action type

--- client/src/reducers/services.js
+++ client/src/reducers/services.js
@@ -2,7 +2,7 @@ import { FETCH_ALL, CREATE, UPDATE, DELETE, FETCH } from '../constants/servicesA
 
 export default (services = [], action) => {
     switch (action.type) {
-        case FETCH_ALL_SERVICES:
+        case FETCH_ALL:
             return action.payload;
         case DELETE:
             return services.filter((service) => service._id !== action.payload);
