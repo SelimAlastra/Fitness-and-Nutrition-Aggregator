0f15097e3c5fee5eac04b00a92117c8cb82a16c3
khalid-alsheeb <khalid.alsheeb@kcl.ac.uk>
2021-03-30 13:13:19 +0300

    small bug fix

--- server/index.js
+++ server/index.js
@@ -45,5 +45,3 @@ mongoose.connect(`${uri}`, { useNewUrlParser: true, useUnifiedTopology: true })
 
 mongoose.set('useFindAndModify', false);
 mongoose.set("useCreateIndex", true);
-
-module.exports = app;
