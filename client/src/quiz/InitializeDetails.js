a9bca9eaaea20ad64bf586a931d2117748e25a5a
khalid-alsheeb <khalid.alsheeb@kcl.ac.uk>
2021-03-30 20:15:16 +0300

    removed redundant files.

--- client/src/quiz/InitializeDetails.js
+++
@@ -1,7 +0,0 @@
-import { details } from './quizUser';
-import { useDispatch, useSelector } from 'react-redux';
-import { getBasicUser, getBasicUsers, updateBasicUser } from '../actions/basicUsers';
-import { getProfessional, getProfessionalUsers } from '../actions/professionals';
-import { createGoal } from '../actions/goals';
-import React,{useEffect} from 'react';
-
