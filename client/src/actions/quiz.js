37f65af4221f68a2b175b89da082a013199f9643
sergiu-stefan <sergiu-stefan.tomescu@kcl.ac.uk>
2021-03-30 14:48:09 +0300

    Quiz.

--- client/src/actions/quiz.js
+++ client/src/actions/quiz.js
@@ -1,7 +1,24 @@
 
+// How to get a specific element from the database?
+// e.g.: the object for a profile's gender/height/weight
+
+// How to edit the properties of that object?
+// e.g.: for the goals table, set the goal values
 
 export const updateClientAnswers = (quiz) => async (dispatch) => {
+
+    // save gender, age, height and weight
+
+    // save the goals
+
+    // save the profile-associated tags
+
 }
 
 export const updateProfessionalAnswers = (quiz) => async (dispatch) => {
+
+    // save gender, age, height and weight
+
+    // save the profile-associated tags
+
 }
