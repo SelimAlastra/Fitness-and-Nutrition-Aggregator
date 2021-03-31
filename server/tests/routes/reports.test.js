4443d4e0ded36f415b32d392e2a561521be620d5
khalid-alsheeb <khalid.alsheeb@kcl.ac.uk>
2021-03-31 17:15:35 +0300

    fixed reports issue, and added a new test

--- server/tests/routes/reports.test.js
+++ server/tests/routes/reports.test.js
@@ -1,86 +1,93 @@
-// import request from 'supertest';
-// import chai from 'chai';
-// const expect = chai.expect;
-// import app from '../../index.js';
-// import Report from '../../models/reports.js'
-// import BasicUser from '../../models/basicUser.model.js'
-// import ProfessionalUser from '../../models/professionalUser.model.js'
-// import PostMessage from '../../models/postMessage.js'
+import request from 'supertest';
+import chai from 'chai';
+const expect = chai.expect;
+import app from '../../index.js';
+import Report from '../../models/reports.js'
+import BasicUser from '../../models/basicUser.model.js'
+import ProfessionalUser from '../../models/professionalUser.model.js'
+import PostMessage from '../../models/postMessage.js'
 
 
 
-// describe('report routes', () => {
-//     let reporter, reported, reportedPost;
-//     before(() => {
-//         reporter = new BasicUser({
-//             username: 'Bob_123',
-//             email: 'bob@hotmail.com',
-//             password: 'bOb@123123',
-//             name: 'Bob'})
-//     });
+describe('report routes', () => {
+    let reporter, reported, reportedPost;
+    before((done) => {
+        reporter = new BasicUser({
+            username: 'Bob_123',
+            email: 'bob@hotmail.com',
+            password: 'bOb@123123',
+            name: 'Bob'
+        })
+        reported = new ProfessionalUser({
+            username: 'Alice_123',
+            email: 'alice@hotmail.com',
+            password: 'aliCe@123123',
+            name: 'Alice'
+        })
+        reportedPost = new PostMessage({
+            creator: reported.username,
+            userFrom: reported._id,
+            title: 'Test Post'
+        })
+        done()
+    });
 
-//     before(() => {
-//         reported = new ProfessionalUser({
-//             username: 'Alice_123',
-//             email: 'alice@hotmail.com',
-//             password: 'aliCe@123123',
-//             name: 'Alice'})
-//     });
+    describe('post /reports', () => {
+        it('should make a new report', function(done) {
+            request(app)
+            .post('/reports')
+            .send({
+                reporterUsername: reporter.username,
+                reportedUsername: reported.username,
+                postId: reportedPost._id,
+                reason: 'Test Report'
+            })
+            .end((err, res) => {
+                expect(res.status).to.equal(200);
+                expect(res.body.reason).to.equal('Test Report');
+                expect(res.body).to.have.property("reporterUsername");
+                expect(res.body).to.have.property('reporterUsername')
+                expect(res.body.reporterUsername).to.eq(reporter.username)
+                expect(res.body).to.have.property('reportedUsername')
+                expect(res.body.reportedUsername).to.eq(reported.username)
+                expect(res.body).to.have.property('postId')
+                expect(res.body.postId).to.eq("" +reportedPost._id)
+                done();
+            })
+        })
 
+        it('should return a 409 if the report could not be added', function(done) {
+            request(app)
+            .post('/posts')
+            .send({
+                reason: 'Test Report'
+            })
+            .end((err, res) => {
+                expect(res.status).to.equal(409);
+            });
+            done();
+        });
+    });
 
-//     before((done) => {
-//         reportedPost = new PostMessage({
-//             creator: reported.username,
-//             userFrom: reported._id,
-//             title: 'Test Post'
-//         })
-//         done()
-//     });
+    describe('get /reports', function() {
 
+        it('should get all reports and return a 200 status code and an array', function(done) {
+            request(app)
+            .get('/reports')
+            .send()
+            .end((err, res) => {
+                expect(res.status).to.equal(200);
+                expect(res.body).to.be.array();
+            });
+            done();
+        });
+    });
 
-//     let report;
-//     describe('post /reports', () => {
-//         it('should make a new report', (done) => {
-//             request(app)
-//             .post('/reports')
-//             .send({
-//                 reporterUsername: reporter.username,
-//                 reportedUsername: reported.username,
-//                 postId: reportedPost._id,
-//                 reason: 'Test Report'
-//             })
-//             .end((err, res) => {
-//                 report = res.body
-//                 expect(res.status).to.equal(201);
-//                 expect(res.body.reason).to.equal('Test Report');
-//                 expect(report).to.have.property("reporterUsername");
-//                 expect(report.reporterUsername).to.equal(reporter.username);
-//                 expect(report).to.have.property("reportedUsername");
-//                 expect(report.reportedUsername).to.equal(reported.username);
-//                 expect(report).to.have.property("postId");
-//                 expect(report.postId).to.equal("" + reportedPost._id);
-//                 done();
-//             })
-//         })
-//         // it('should have the reporter username', () => {
-//         //     expect(report).to.have.property(reporterUsername)
-//         //     expect(report.reporterUsername).to.eq(reporter.username)
-//         // })
-//         // it('should have the reported username', () => {
-//         //     expect(report).to.have.property(repotedUsername)
-//         //     expect(report.repotedUsername).to.eq(reported.username)
-//         // })
-//         // it('should have the reported post id', () => {
-//         //     expect(report).to.have.property(postId)
-//         //     expect(report.postId).to.eq(reportedPost._id)
-//         // })
-//     })
-
-//     after((done) => {
-//         Report.deleteMany({});
-//         BasicUser.deleteMany({});
-//         ProfessionalUser.deleteMany({});
-//         PostMessage.deleteMany({});
-//         done()
-//     });
-// });
+    after((done) => {
+        Report.deleteMany({});
+        BasicUser.deleteMany({});
+        ProfessionalUser.deleteMany({});
+        PostMessage.deleteMany({});
+        done()
+    });
+});
