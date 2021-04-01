df37b70698d775676b4c0c449826df8e0662b8cf
khalid-alsheeb <khalid.alsheeb@kcl.ac.uk>
2021-04-01 04:28:45 +0300

    removed most of the admin routes for safety.

--- server/routes/admins.js
+++ server/routes/admins.js
@@ -1,11 +1,10 @@
 import express from 'express';
-import { getAdmins, loginController } from "../controllers/admins.js";
+import { loginController } from "../controllers/admins.js";
 
 const router = express.Router();
 
-//Other controllers are not pasrt of the API for safety concers.
+//Other routes removed for safety.
 
-router.get('/', getAdmins);
 router.post('/login', loginController)
 
 export default router;
