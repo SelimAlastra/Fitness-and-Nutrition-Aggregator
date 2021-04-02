3439d51c7d8bf0a632836b84b3d83a3040aecbfa
Tushita Yadav <tushita.yadav@kcl.ac.uk>
2021-04-02 15:47:59 +0100

    Fixed search and form tests, admin test needs to be completed

--- client/cypress/integration/professionaluserlogin.spec.js
+++ client/cypress/integration/professionaluserlogin.spec.js
@@ -18,4 +18,25 @@ describe('Test', function() {
         cy.contains('LOGO');
     });
 
+    it('Form functionality Test', function() {
+
+        cy.visit('http://localhost:3000/');
+        cy.contains('Professionals').should('be.visible');
+        cy.findByTestId('professionalUsersButton').click();
+        cy.focused();
+        cy.contains('Log In').should('exist');
+        cy.contains('Log In').click();
+        cy.get('input[placeholder="Enter your email"]').type('markkane@yahoo.com');
+        cy.get('input[placeholder="Enter your password"]').type('kane1234'); 
+        cy.get('button').contains('Log In').click();
+        cy.url().should('include', '/professionalDashboard/6063969add5a59ea908e4cb9');
+        cy.get('input[name="title"]').type('Fitness plan');
+        cy.get('input[name="message"]').type('Visit my profile for more information');
+        cy.get('input[name="tags"]').type('fitness,plan,healthy');
+        cy.get('button').contains('Submit').click();
+        cy.get('input[placeholder="Search…').type('fitness');
+        cy.contains('#fitness').should('exist');
+        cy.contains('#plan').should('exist');
+    }); 
+
 });
