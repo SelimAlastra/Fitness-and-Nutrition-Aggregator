0dad5fdf51ea51ed36f97440b535b74e41557dc5
Tushita Yadav <tushita.yadav@kcl.ac.uk>
2021-04-02 12:10:08 +0100

    Search Test

--- client/cypress/integration/basicuserlogin.spec.js
+++ client/cypress/integration/basicuserlogin.spec.js
@@ -15,6 +15,8 @@ describe('Test', function() {
         cy.get('input[placeholder="Enter your password"]').type('john1234'); 
         cy.get('button').contains('Log In').click();
         cy.url().should('include', '/clientDashboard/60639559dd5a59ea908e4cb7'); 
+        cy.get('input[placeholder="Search…"]').focus().click().type('{enter}abs');
+        cy.contains('10 minute ABS WORKOUT').should('exist');
     });
 
     it('Basic User Dashboard Test', function() {
@@ -22,8 +24,24 @@ describe('Test', function() {
         cy.contains('LOGO');
     });
 
-  it('Searchbox functionality Test', function() {            
+    /* 
+        Note: Due to secure routing implemented in the application, integration tests seem to 
+        require starting from the login to reach the functionality we are trying to test.
+        Need to find workaround.
+    */
+  it('Searchbox functionality Test', function() { 
+        //cy.visit('http://localhost:3000/clientDashboard/60639559dd5a59ea908e4cb7');           
         //cy.get('input[data-testid="searchBox"]').click({force: true}).type('{enter}abs');
+        cy.visit('http://localhost:3000/');
+        cy.contains('Basic Users').should('be.visible');
+        cy.findByTestId('basicUsersButton').click();
+        cy.focused();
+        cy.contains('Log In').should('exist');
+        cy.contains('Log In').click();
+        cy.get('input[placeholder="Enter your email"]').type('johnthompson@yahoo.com');
+        cy.get('input[placeholder="Enter your password"]').type('john1234'); 
+        cy.get('button').contains('Log In').click();
+        cy.url().should('include', '/clientDashboard/60639559dd5a59ea908e4cb7'); 
         cy.get('input[placeholder="Search…"]').focus().click().type('{enter}abs');
         cy.contains('10 minute ABS WORKOUT').should('exist');
     }); 
