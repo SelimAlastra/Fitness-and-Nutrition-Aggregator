f3732542e1523ef2ee50fa1f539733fcd8fc6d9c
Tushita Yadav <tushita.yadav@kcl.ac.uk>
2021-04-02 13:16:35 +0100

    Added seed data in fixtures

--- client/cypress/integration/basicuserregister.spec.js
+++ client/cypress/integration/basicuserregister.spec.js
@@ -1,5 +1,11 @@
 describe('Test', function() {
 
+    before(function () {
+        cy.fixture('basicUser').then(function(basicUser){
+            this.basicUser = basicUser
+        })
+    })
+
     it('Basic User Register Test', function() {
         cy.visit('http://localhost:3000/');
         cy.contains('Basic Users').should('be.visible');
@@ -7,12 +13,11 @@ describe('Test', function() {
         cy.focused();
         cy.contains('SIGN UP').should('exist');
         cy.contains('SIGN UP').click();
-        cy.get('input[placeholder="Enter your email"]').type('basicUsersTest@gmail.com');
-        cy.get('input[placeholder="Insert your name here"]').type('basicUsersTest');
-        cy.get('input[placeholder="Create your username"]').type('basicUsersTest');
-        //cy.get('input[placeholder="Enter your profession here"]').type('johnthompson@yahoo.com');
-        cy.get('input[placeholder="Enter your password"]').type('test1234');
-        cy.get('input[placeholder="Re-enter your password"]').type('test1234');  
+        cy.get('input[placeholder="Enter your email"]').type(this.basicUser.email);
+        cy.get('input[placeholder="Insert your name here"]').type(this.basicUser.name);
+        cy.get('input[placeholder="Create your username"]').type(this.basicUser.username);
+        cy.get('input[placeholder="Enter your password"]').type(this.basicUser.password);
+        cy.get('input[placeholder="Re-enter your password"]').type(this.basicUser.password);  
         cy.get('button').contains('Register').click();
         //cy.url().should('include', '/user/quiz/'); 
         //cy.location('pathname').should('eq', '/user/quiz/:id')
--- client/cypress/integration/professionaluserregister.spec.js
+++ client/cypress/integration/professionaluserregister.spec.js
@@ -1,5 +1,11 @@
 describe('Test', function() {
 
+    before(function () {
+        cy.fixture('professionalUser').then(function(professionalUser){
+            this.professionalUser = professionalUser
+        })
+    })
+
     it('Basic User Register Test', function() {
         cy.visit('http://localhost:3000/');
         cy.contains('Professionals').should('be.visible');
@@ -7,12 +13,12 @@ describe('Test', function() {
         cy.focused();
         cy.contains('SIGN UP').should('exist');
         cy.contains('SIGN UP').click();
-        cy.get('input[placeholder="Enter your email"]').type('professionalUsersTest@gmail.com');
-        cy.get('input[placeholder="Insert your name here"]').type('professionalUsersTest');
-        cy.get('input[placeholder="Create your username"]').type('professionalUsersTest');
-        cy.get('input[placeholder="Enter your profession here"]').type('Test');
-        cy.get('input[placeholder="Enter your password"]').type('test1234');
-        cy.get('input[placeholder="Re-enter your password"]').type('test1234');  
+        cy.get('input[placeholder="Enter your email"]').type(this.professionalUser.email);
+        cy.get('input[placeholder="Insert your name here"]').type(this.professionalUser.name);
+        cy.get('input[placeholder="Create your username"]').type(this.professionalUser.username);
+        cy.get('input[placeholder="Enter your profession here"]').type(this.professionalUser.profession);
+        cy.get('input[placeholder="Enter your password"]').type(this.professionalUser.password);
+        cy.get('input[placeholder="Re-enter your password"]').type(this.professionalUser.password);  
         cy.get('button').contains('Register').click();
     });
 });
