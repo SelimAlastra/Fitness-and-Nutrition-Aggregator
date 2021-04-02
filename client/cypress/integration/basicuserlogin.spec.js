78bd565aaea94160a9abc84838b3074819f59dcc
Tushita Yadav <tushita.yadav@kcl.ac.uk>
2021-04-02 15:47:59 +0100

    Fixed search and form tests, admin test needs to be completed

--- client/cypress/integration/basicuserlogin.spec.js
+++ client/cypress/integration/basicuserlogin.spec.js
@@ -15,13 +15,6 @@ describe('Test', function() {
         cy.get('input[placeholder="Enter your password"]').type('john1234'); 
         cy.get('button').contains('Log In').click();
         cy.url().should('include', '/clientDashboard/60639559dd5a59ea908e4cb7'); 
-        cy.get('input[placeholder="Search…"]').focus().click().type('{enter}abs');
-        cy.contains('10 minute ABS WORKOUT').should('exist');
-    });
-
-    it('Basic User Dashboard Test', function() {
-        cy.visit('http://localhost:3000/clientDashboard/60639559dd5a59ea908e4cb7');
-        cy.contains('LOGO');
     });
 
     /* 
@@ -30,8 +23,6 @@ describe('Test', function() {
         Need to find workaround.
     */
   it('Searchbox functionality Test', function() { 
-        //cy.visit('http://localhost:3000/clientDashboard/60639559dd5a59ea908e4cb7');           
-        //cy.get('input[data-testid="searchBox"]').click({force: true}).type('{enter}abs');
         cy.visit('http://localhost:3000/');
         cy.contains('Basic Users').should('be.visible');
         cy.findByTestId('basicUsersButton').click();
@@ -46,25 +37,4 @@ describe('Test', function() {
         cy.contains('10 minute ABS WORKOUT').should('exist');
     }); 
 
-    /* it('Professional Login Test', function() {
-        cy.visit('http://localhost:3000/');
-        cy.contains('Professionals').should('be.visible');
-        cy.get('button').contains('Professionals').click();
-        cy.focused();
-        cy.contains('Log In').should('exist');
-        cy.contains('Log In').click();
-        cy.get('input[placeholder="Enter your email"]').type('markkane@yahoo.com');
-        cy.get('input[placeholder="Enter your password"]').type('kane1234'); 
-        cy.get('button').contains('Log In').click();
-        cy.url().should('include', '/professionalDashboard/6063969add5a59ea908e4cb9'); 
-    });
- */
-    /* it('Form functionality Test', function() {
-            cy.get('input[name="title"]').type('Fitness plan');
-            cy.get('#form').get('input[name="message"]').type('Visit my profile for more information');
-            cy.get('input[name="tags"]').type('fitness,plan,healthy');
-            cy.get('button').contains('Submit').click();
-            cy.get('input[placeholder="Searching…"]').type('fitness');
-            cy.get('Fitness plan').should('exist');
-    });  */
 });
