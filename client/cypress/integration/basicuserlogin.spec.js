8d3ce791f1f497284cee90d4c05f4b235be07ebe
Tushita Yadav <tushita.yadav@kcl.ac.uk>
2021-04-02 19:56:37 +0100

    Modified existing tests

--- client/cypress/integration/basicuserlogin.spec.js
+++ client/cypress/integration/basicuserlogin.spec.js
@@ -4,8 +4,8 @@ describe('Test', function() {
         return false;
        });
 
-    it('Basic User Login Test', function() {
-        cy.visit('http://localhost:3000/');
+    beforeEach(() => {
+        cy.visit('/');
         cy.contains('Basic Users').should('be.visible');
         cy.findByTestId('basicUsersButton').click();
         cy.focused();
@@ -14,25 +14,33 @@ describe('Test', function() {
         cy.get('input[placeholder="Enter your email"]').type('johnthompson@yahoo.com');
         cy.get('input[placeholder="Enter your password"]').type('john1234'); 
         cy.get('button').contains('Log In').click();
+        cy.url().should('include', '/clientDashboard/60639559dd5a59ea908e4cb7');
+      });
+
+    it('Basic User Login Test', function() {
+        cy.visit('/');
+        cy.contains('Basic Users').should('be.visible');
+        cy.findByTestId('basicUsersButton').click();
+        cy.focused();
+        cy.contains('Log In').should('exist');
+        cy.contains('Log In').click();
+        cy.get('input[placeholder="Enter your email"]').type('johnthompson@yahoo.com');
+        cy.get('input[placeholder="Enter your password"]').type('john1234'); 
+        cy.get('button').contains('Log In').click();
+        // should be redirected to the clientDashboard
         cy.url().should('include', '/clientDashboard/60639559dd5a59ea908e4cb7'); 
+        // auth cookie should be present
+       //cy.getCookie('XSRF-TOKEN').should('exist')
     });
 
     /* 
         Note: Due to secure routing implemented in the application, integration tests seem to 
         require starting from the login to reach the functionality we are trying to test.
         Need to find workaround.
+        Current Solution: Run login before each test
+        cy.request() might help solve this
     */
   it('Searchbox functionality Test', function() { 
-        cy.visit('http://localhost:3000/');
-        cy.contains('Basic Users').should('be.visible');
-        cy.findByTestId('basicUsersButton').click();
-        cy.focused();
-        cy.contains('Log In').should('exist');
-        cy.contains('Log In').click();
-        cy.get('input[placeholder="Enter your email"]').type('johnthompson@yahoo.com');
-        cy.get('input[placeholder="Enter your password"]').type('john1234'); 
-        cy.get('button').contains('Log In').click();
-        cy.url().should('include', '/clientDashboard/60639559dd5a59ea908e4cb7'); 
         cy.get('input[placeholder="Search…"]').focus().click().type('{enter}abs');
         cy.contains('10 minute ABS WORKOUT').should('exist');
     }); 
