5d10cf8b791407c2f5dd50e97816f6eb0adc98b5
Sergiu Stefan Tomescu <72317577+sergiu-stefan@users.noreply.github.com>
2021-04-06 18:22:51 +0300

    Functional dob question.

--- client/src/quiz/components/dobPicker.js
+++ client/src/quiz/components/dobPicker.js
@@ -3,29 +3,34 @@ import DatePicker from "react-datepicker";
 
 import "react-datepicker/dist/react-datepicker.css";
 
+/**
+ * 
+ * @returns a general date picker in the format dd/MM/yyy
+ */
 const DobPicker = () => {
   const [startDate, setStartDate] = useState(new Date());
   return (
     <div className="dobPicker">
     <DatePicker 
+    id="DatePicker"
+    dateFormat="dd/MM/yyyy"
     required
-                    selected={startDate}
-                    onChange={date => setStartDate(date)} 
-                    dateFormat="Pp"
-                    className="Datepicker pa2"
-                    placeholderText="Select a date"
-                    calendarClassName="rasta-stripes"
-                    popperModifiers={{
-                        offset: {
-                          enabled: true,
-                          offset: "0px, 0px"
-                        },
-                        preventOverflow: {
-                          enabled: true,
-                          escapeWithReference: false,
-                          boundariesElement: "scrollParent"
-                        }
-                      }}
+    selected={startDate}
+    onChange={date => setStartDate(date)} 
+    className="Datepicker pa2"
+    placeholderText="Select a date"
+    calendarClassName="rasta-stripes"
+    popperModifiers={{
+        offset: {
+          enabled: true,
+          offset: "0px, 0px"
+        },
+        preventOverflow: {
+          enabled: true,
+          escapeWithReference: false,
+          boundariesElement: "scrollParent"
+        }
+      }}
     />
     </div>
   );
