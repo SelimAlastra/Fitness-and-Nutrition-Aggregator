9b2c546a913f53cb2135f0b39b140cb7d3f1af89
Selim Alastra <selim.alastra@kcl.ac.uk>
2021-04-06 19:03:25 +0100

    Fixes background in BucketsPage

--- client/src/components/Buckets2/BucketsPage.js
+++ client/src/components/Buckets2/BucketsPage.js
@@ -28,13 +28,13 @@ const Buckets = () => {
 
     if (myBuckets === undefined || myBuckets.length === 0) {
         return (
-            <div>
+            <div style={{backgroundColor: "whitesmoke", height :"100vh"}}>
             <NavbarUser/>
             <div className="bucketText">
                     <h1>My Buckets</h1>
                     <hr className="serviceSeperator"/>
                 </div>
-                <div style = {{marginLeft:"-60%", marginTop: "2%"}}><BucketModal/></div>
+                <div style = {{margin: "20px"}}><BucketModal/></div>
                 <div>You have no buckets.</div>
                 <div>Create buckets and save your preferred posts for later.</div>
             </div>
@@ -42,13 +42,13 @@ const Buckets = () => {
     }
     else {
         return (
-            <div style={{backgroundColor: "whitesmoke"}}>
+            <div style={{backgroundColor: "whitesmoke", height :"100vh"}}>
                 <NavbarUser/>
                 <div className="bucketText">
                     <h1>My Buckets</h1>
                     <hr className="serviceSeperator"/>
                 </div>
-                <div style = {{marginTop: "2%"}}><BucketModal/></div>
+                <div style = {{margin: "20px"}}><BucketModal/></div>
                 <div class="container" style={{justifyContent: "center"}}>
                 <div class="row">
                         {myBuckets.map((bucket) => (
