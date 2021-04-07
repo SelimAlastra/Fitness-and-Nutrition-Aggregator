0834e97fe9438a1ba7970b8b125c502c8cf191cd
khalid-alsheeb <khalid.alsheeb@kcl.ac.uk>
2021-04-07 23:28:47 +0300

    updated api urls, in /api/index

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,19 +1,13 @@
 import axios from 'axios';
 
 const API= axios.create({baseURL:'http://localhost:5000'});
-const url = 'http://localhost:5000/posts';
-const reportUrl = 'http://localhost:5000/reports';
-const professionalUsersUrl = 'http://localhost:5000/professionalUsers';
-const servicesUrl = 'http://localhost:5000/services';
-const basicUserUrl = 'http://localhost:5000/basicUsers';
-const bucketUrl = 'http://localhost:5000/buckets';
-const goalUrl = 'http://localhost:5000/goals';
 
+export const baseUrl = 'http://localhost:5000';
 
-export const fetchReports = () => axios.get(reportUrl);
-export const createReport = (newReport) => axios.post(reportUrl, newReport);
-export const deleteReport = (id) => axios.delete(reportUrl+'/'+id);
-export const getReport = (id) => axios.get(`${reportUrl}/${id}`);
+export const fetchReports = () => API.get('/reports');
+export const createReport = (newReport) => API.post('/reports', newReport);
+export const deleteReport = (id) => API.delete(`/reports/${id}`);
+export const getReport = (id) => API.get(`/reports/${id}`);
 
 export const fetchPosts = () => API.get('/posts');
 export const createPost = (newPost) => API.post('/posts', newPost);
@@ -21,35 +15,34 @@ export const updatePost = (id, updatedPost) =>API.patch(`/posts/${id}`, updatedP
 export const deletePost = (id) => API.delete(`/posts/${id}`);
 export const likePost = (id,userId) => API.patch(`/posts/${id}/${userId}/likePost`);
 export const toggleFavAction = (id) => API.patch(`/posts/${id}/toggleFavAction`);
-export const getPost = (id) => axios.get(`${url}/${id}`);
-export const getPostsFromArray = (bucketId) => axios.get(`${url}/${bucketId}/bucket`);
+export const getPost = (id) => API.get(`/posts/${id}`);
+export const getPostsFromArray = (bucketId) => API.get(`/posts/${bucketId}/bucket`);
 
-export const getProfessional = (id) => axios.get(`${professionalUsersUrl}/${id}`);
-export const updateProfessional = (id, updatedProfile) => axios.patch(`${professionalUsersUrl}/update/${id}`, updatedProfile);
-export const fetchProfessionalUsers = () => axios.get(professionalUsersUrl);
-export const createProfessionalUser = (newUser) => axios.post(professionalUsersUrl, newUser);
-export const deleteProfessionalUser = (id) => axios.delete(`${professionalUsersUrl}/${id}`);
+export const getProfessional = (id) => API.get(`/professionalUsers/${id}`);
+export const updateProfessional = (id, updatedProfile) => API.patch(`/professionalUsers/update/${id}`, updatedProfile);
+export const fetchProfessionalUsers = () => API.get('/professionalUsers');
+export const createProfessionalUser = (newUser) => API.post('/professionalUsers', newUser);
+export const deleteProfessionalUser = (id) => API.delete(`/professionalUsers/${id}`);
 
-export const getServices = () => axios.get(servicesUrl);
-export const deleteService = (id) => axios.delete(`${servicesUrl}/${id}`)
-export const addService = (service) => axios.post(`${servicesUrl}/add`, service);
-export const updateService = (id, updatedService) => axios.patch(`${servicesUrl}/update/${id}`, updatedService);
+export const getServices = () => API.get('/services');
+export const deleteService = (id) => API.delete(`/services/${id}`)
+export const addService = (service) => API.post(`/services/add`, service);
+export const updateService = (id, updatedService) => API.patch(`/services/update/${id}`, updatedService);
 
-export const getBasicUser = (id) => axios.get(`${basicUserUrl}/${id}`);
-export const updateBasicUser = (id, updatedBasicUser) => axios.patch(`${basicUserUrl}/update/${id}`, updatedBasicUser);
+export const getBasicUser = (id) => API.get(`/basicUsers/${id}`);
+export const updateBasicUser = (id, updatedBasicUser) => API.patch(`/basicUsers/update/${id}`, updatedBasicUser);
+export const fetchBasicUsers = () => API.get('/basicUsers');
+export const createBasicUser = (newUser) => API.post('/basicUsers', newUser);
+export const deleteBasicUser = (id) => API.delete(`/basicUsers/${id}`);
 
-export const getGoals = () => axios.get(goalUrl);
-export const getGoal = (id) => axios.get(`${goalUrl}/${id}`);
-export const updateGoal= (id, updatedGoal) => axios.patch(`${goalUrl}/${id}`, updatedGoal);
-export const deleteGoal = (id) => axios.delete(`${goalUrl}/${id}`)
+export const getGoals = () => API.get('/goals');
+export const getGoal = (id) => API.get(`/goals/${id}`);
+export const updateGoal= (id, updatedGoal) => API.patch(`/goals/${id}`, updatedGoal);
+export const deleteGoal = (id) => API.delete(`/goals/${id}`)
 export const createGoal = (newGoal) => API.post('/goals', newGoal);
 
-export const fetchBasicUsers = () => axios.get(basicUserUrl);
-export const createBasicUser = (newUser) => axios.post(basicUserUrl, newUser);
-export const deleteBasicUser = (id) => axios.delete(`${basicUserUrl}/${id}`);
-
-export const getBuckets = () => axios.get(bucketUrl);
-export const createBucket = (newBucket) => axios.post(bucketUrl, newBucket);
-export const updateBucket = (id, updatedBucket) => axios.patch(`${bucketUrl}/${id}`, updatedBucket);
-export const getBucket = (id) => axios.get(`${bucketUrl}/${id}`);
-export const deleteBucket = (id) => axios.delete(`${bucketUrl}/${id}`);
+export const getBuckets = () => API.get('/buckets');
+export const createBucket = (newBucket) => API.post('/buckets', newBucket);
+export const updateBucket = (id, updatedBucket) => API.patch(`/buckets/${id}`, updatedBucket);
+export const getBucket = (id) => API.get(`/buckets/${id}`);
+export const deleteBucket = (id) => API.delete(`/buckets/${id}`);
