720796f0e1f882c26401e4528554c5aeb36ae1e7
khalid-alsheeb <khalid.alsheeb@kcl.ac.uk>
2021-04-08 13:11:13 +0300

    deleted redundant files

--- client/src/ProfessionalDashboard.js
+++ client/src/ProfessionalDashboard.js
@@ -1,25 +1,11 @@
 
 import React, { useEffect ,useState } from 'react';
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Toolbar from "@material-ui/core/Toolbar";
+import { Container, Grow, Grid } from '@material-ui/core';
 import NavbarProfessional from "./components/Navbar/NavbarProfessional";
-import List from "@material-ui/core/List";
-import Divider from "@material-ui/core/Divider";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import InboxIcon from "@material-ui/icons/MoveToInbox";
-import MailIcon from "@material-ui/icons/Mail";
 import { useDispatch } from 'react-redux';
-import { getPosts, updatePost } from './actions/posts';
-import { getProfessionalUsers } from './actions/professionals';
+import { getPosts } from './actions/posts';
 import Posts from './components/Posts/Posts';
-import Form from './components/Form/Form';
-import memories from './images/memories.png';
 import useStyles from './styles';
-import { useSelector } from 'react-redux';
-import { getProfessional } from './actions/professionals';
 
 const ProfessionalDashboard = () => {
     const [currentId, setCurrentId] = useState(null);
